6dd6bd5f6aa740fa688b8116d42644cff8a8534d
GOD <god@Heaven.local>
2017-03-12 22:59:37 +0100

    Initial Commit

---
+++ AbdoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AbdoApp
+//
+//  Created by GOD on 12/03/2017.
+//  Copyright © 2017 GOD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
